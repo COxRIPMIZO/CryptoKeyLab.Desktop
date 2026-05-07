0a7b01f3d8560443fc6a2f537900d36deabe05f9
THAxSENSEI <THAxSENSEI@THAxSENSEI.COM>
2026-05-07 23:31:54 +0530

    Add project files.

---
+++ CryptoKeyLab.Desktop/Services/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoKeyLab.Desktop.Services
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        // ReSharper disable once EventNeverSubscribedTo.Global
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // ReSharper disable once UnusedMember.Global
+        public void OnPropertyChanged(string propName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
